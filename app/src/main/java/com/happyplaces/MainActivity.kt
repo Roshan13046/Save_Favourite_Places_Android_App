2971220ddb52dde212906cdf5217a21210010384
Roshan Prakash Raut <roshanpk.raut@gmail.com>
2021-01-26 19:09:46 +0530

    Home Screen and Favourite Places Screen Added.

---
+++ app/src/main/java/com/happyplaces/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.happyplaces
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_main)
+        // START
+        // Setting an click event for Fab Button and calling the AddHappyPlaceActivity.
+        fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+        // END
+    }
+}
